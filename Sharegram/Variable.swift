f762e54bc231297e745e14d7cb5aaee5fff53785
SatGatLee <step323@naver.com>
2018-01-10 15:22:00 +0900

    1

---
+++ Sharegram/Variable.swift
@@ -0,0 +1,18 @@
+//
+//  Variable.swift
+//  Sharegram
+//
+//  Created by apple on 2018. 1. 10..
+//  Copyright © 2018년 박주영. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import UIKit
+
+class variable {
+    var ref : DatabaseReference?
+    var PasswordText : UITextField?
+    var EmailText : UITextField?
+    
+}
