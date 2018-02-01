e5bb6ef562a1cbd7fb2f6d38ba1c2ff64573c41e
SatGatLee <step323@naver.com>
2018-02-01 16:20:20 +0900

    ㅇ

--- Sharegram/WriteViewController.swift
+++ Sharegram/WriteViewController.swift
@@ -9,29 +9,37 @@
 import UIKit
 import Firebase
 import SnapKit
+import Popover
 
-class WriteViewController: UIViewController, UITextViewDelegate,UIPopoverPresentationControllerDelegate, dataDelegate {
-    
-    func TransferData(_ array : String) {
-        print("array")
-        self.writeDescription.text = array
-    }
-    
+class WriteViewController: UIViewController{
     var writeImage : UIImage!
     var baseString : String! // 이미지 데이터 변환 포맷
     var object = variable()
     var ref : DatabaseReference?
     var storageRef : StorageReference?
-    var PostArray : [[String : String]] = []
+    var PostArray : [String : String] = [:]
     var Hash : [AnyToken]!
     var HashTagArray : [String] = []
-    var str : String = ""
-
+    var strr : String = ""
+    var stringArray : [String] = []
+    var result : [String] = []
+    var resultString : String = ""
+    var HashTagview = UITableView()
+    
+    fileprivate var texts = ["Edit", "Delete", "Report"]
+    
+    fileprivate var popover: Popover!
+    fileprivate var popoverOptions: [PopoverOption] = [
+        .type(.down),
+        .blackOverlayColor(UIColor(white: 0.0, alpha: 0.6))
+    ]
+    
     @IBOutlet weak var writeimageView: UIImageView!
     @IBOutlet weak var writeDescription: UITextView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var LocationSwitch: UISwitch!
     @IBOutlet weak var writeBut: UIButton!
+    
     @IBAction func ActBut(_ sender: UIButton) { // 작성 버튼을 눌렀을 때
         let date = Date()
         let calendar = Calendar.current
@@ -43,26 +51,34 @@ class WriteViewController: UIViewController, UITextViewDelegate,UIPopoverPresent
         let hour = components.hour!
         let min = components.minute!
         let dateString = "\(year)년\(month)월\(day)일\(hour)시\(min)분"
-        let ss = "PostImage/\((Auth.auth().currentUser?.email)!)/\(dateString).png"
+        let ImagePath = "PostImage/\((Auth.auth().currentUser?.email)!)/\(dateString).png"
         
         if LocationSwitch.isOn { //위치 공유 허용 상태이면 즉 On 상태일 때, 카메라로 사진을 찍어서 가져왔을 때
-            DataSave(ss, dateString, identifier: 0)
+
+            DataSave(ImagePath, dateString, identifier: 0)
         } else { //단순히 라이브러리 사진을 게시글로 작성할 때, 아니면 자신의 위치를 공유하지 않을 때
             if writeImage == nil { //그냥 글만 쓸 때
                 print("nil")
                 self.Hash = self.writeDescription.text._tokens(from: HashtagTokenizer())
                 self.NumberOfHasgTag(self.Hash)
-                self.PostArray.append(["Author" : (Auth.auth().currentUser?.displayName)!, "Description" : self.writeDescription.text, "Date" : dateString])
-                self.CountUpHasgTag()
-                self.ref?.child("User").child((Auth.auth().currentUser?.uid)!).child("Posts").setValue(self.PostArray)
-                self.displayErrorMessage(title: "게시물이", message: "등록되었습니다!")
-            } else { // 사진이 있는데 라이브러리사진인 경우
+                self.PostArray = ["Author" : (Auth.auth().currentUser?.displayName)!, "Description" : self.writeDescription.text, "Date" : dateString, "ID" : (Auth.auth().currentUser?.uid)!]
+                SubFuncDataSave()
+            } else { // 사진이 있는데 라이브러리사진인 경우, 위치를 가져오나 공유하기 싫을 때
                 print("??")
-                DataSave(ss, dateString, identifier: 1)
+                DataSave(ImagePath, dateString, identifier: 1)
             }
         }
     }
-    func DataSave(_ Path : String, _ date : String, identifier : Int) {
+    func SubFuncDataSave() {
+        self.CountUpHasgTag()
+        self.ref?.child("WholePosts").childByAutoId().setValue(self.PostArray) // 전체 게시물 등록
+        self.ref?.child("User").child((Auth.auth().currentUser?.uid)!).child("Posts").childByAutoId().setValue(self.PostArray) {(error, ref) -> Void in // 유저 자신의 게시물에 등록
+            if error == nil { //완성 됐을 때
+                self.displayErrorMessage(title: "게시물이", message: "등록되었습니다!")
+            }
+        }
+    }
+    func DataSave(_ Path : String, _ date : String, identifier : Int) { // 데이터 저장
         let uploadImage = UIImageJPEGRepresentation(writeImage, 0.9)!
         let metadata1 = StorageMetadata()
         metadata1.contentType = "image/jpeg"
@@ -75,19 +91,21 @@ class WriteViewController: UIViewController, UITextViewDelegate,UIPopoverPresent
                 self.Hash = self.writeDescription.text._tokens(from: HashtagTokenizer())
                 self.NumberOfHasgTag(self.Hash)
                 
-                if identifier == 0 {
-                    self.PostArray.append(["image" : Path,"latitude" : "\(self.object.lat)", "longitude" : "\(self.object.lon)", "Author" : (Auth.auth().currentUser?.displayName)!, "Description" : self.writeDescription.text, "Date" : date])
-                } else {
+                if identifier == 0 { //위치 공유 할 시
+                    self.PostArray = ["image" : Path,"latitude" : "\(self.object.lat)", "longitude" : "\(self.object.lon)", "Author" : (Auth.auth().currentUser?.displayName)!, "Description" : self.writeDescription.text, "Date" : date, "ID" : (Auth.auth().currentUser?.uid)!]
+                                self.ref?.child("LocationPosts").child(<#T##pathString: String##String#>)
+                } else { // 1
                     
-                    self.PostArray.append(["image" : Path, "Author" : (Auth.auth().currentUser?.displayName)!, "Description" : self.writeDescription.text, "Date" : date])
+                    self.PostArray = ["image" : Path, "Author" : (Auth.auth().currentUser?.displayName)!, "Description" : self.writeDescription.text, "Date" : date, "ID" : (Auth.auth().currentUser?.uid)!]
                 }
-                self.CountUpHasgTag()
-                self.ref?.child("User").child((Auth.auth().currentUser?.uid)!).child("Posts").setValue(self.PostArray)
-                self.displayErrorMessage(title: "게시물이", message: "등록되었습니다!")
+                self.SubFuncDataSave()
+//                self.CountUpHasgTag()
+//                self.ref?.child("User").child((Auth.auth().currentUser?.uid)!).child("Posts").childByAutoId().setValue(self.PostArray)
+//                self.displayErrorMessage(title: "게시물이", message: "등록되었습니다!")
             }
         })
     }
-    func CountUpHasgTag() {
+    func CountUpHasgTag() { // 해쉬태그 카운트 증가 시키기 + 해쉬태그 게시물 등록
         print(self.HashTagArray)
         for i in 0..<self.HashTagArray.count {
             print(self.HashTagArray[i])
@@ -95,102 +113,83 @@ class WriteViewController: UIViewController, UITextViewDelegate,UIPopoverPresent
             ref?.child("HashTagPosts").child(char).child("Count").observeSingleEvent(of: .value, with: { (snapshot) in
                 if snapshot.value is NSNull {
                     print("존재하지 않습니다.")
-                    self.ref?.child("HashTagPosts").child(char).child("Count").setValue(["Count" : "1"])
+                    self.ref?.child("HashTagPosts").child(char).child("Count").updateChildValues(["Count" : "1"])
                     self.ref?.child("HashTagPosts").child(char).child("Count").setValue(["Name" : char])
-                    self.ref?.child("HashTagPosts").child(char).child("Posts").setValue(self.PostArray)
+                    self.ref?.child("HashTagPosts").child(char).child("Posts").childByAutoId().setValue(self.PostArray)
                 } else { //카운트가 1이상일시 즉 하나라도 게시물이 존재한다는 가정
                     if let item = snapshot.value as? [String : String] {
                         var count = Int(item["Count"]!)!
                         count += 1
-                        self.ref?.child("HashTagPosts").child(char).child("Count").setValue(["Count" : "\(count)"])
+                        self.ref?.child("HashTagPosts").child(char).child("Count").updateChildValues(["Count" : "\(count)"])
                         self.ref?.child("HashTagPosts").child(char).child("Count").setValue(["Name" : char])
-                        self.ref?.child("HashTagPosts").child(char).child("Posts").setValue(self.PostArray)
+                        self.ref?.child("HashTagPosts").child(char).child("Posts").childByAutoId().setValue(self.PostArray)
                     }
                 }
             })
         }
     }
-    func NumberOfHasgTag(_ Token : [AnyToken]){
+    func NumberOfHasgTag(_ Token : [AnyToken]){ // 해쉬태그 거르기
         for i in 0..<Token.count {
             self.HashTagArray.append(Token[i].text)
         }
     }
-    func popOver(_ send : UITextView) {
-        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let transfer = storyboard.instantiateViewController(withIdentifier: "HashTag") as! HashTagTableViewController
-        transfer.delegate = self
-        transfer.saveText = self.writeDescription.text
-        let vc = storyboard.instantiateViewController(withIdentifier: "HashTag")
-        vc.modalPresentationStyle = .popover
-        vc.preferredContentSize = CGSize(width: 200, height: 200)
-
-        let popover = vc.popoverPresentationController!
-        popover.delegate = self
-        popover.permittedArrowDirections = .up
-        popover.sourceView = send as UIView
-        popover.sourceRect = send.bounds
-        
-        self.present(vc, animated: true, completion: nil)
-    }
-//    func AutoCollection() {
-//        self.writeDescription.text.mat
-//    }
-    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
-        return .none
-    }
-    func findUsers(text: String){
-        ref?.child("HashTagPosts").child(text).queryOrdered(byChild: "Name").queryEqual(toValue: text+"\u{f8ff}").observe(.value, with: { (snapshot) in
-            if snapshot.value is NSNull {
-                print("Null")
-            } else {
-                print(snapshot.value)
-                for i in snapshot.children {
-                    print(i)
-                }
-            }
-        })
-    }
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if text == "#" { //텍스트 뷰에 #이 적힐 때 마다
-            print("HashTag!!!!!")
-            popOver(textView)
-            //AutoCollection()
-        }
-        if textView.text == "설명 입력" {
-            textView.text = ""
-            textView.tintColor = UIColor.black
-        }
-        if (text == "\n") {
-            textView.endEditing(true)
-            return false
-        }
-        return true
-    }
-    @objc func dismisskeyboard() {
+    @objc func dismisskeyboard() {//디스미스
         writeDescription.resignFirstResponder()
     }
-    @objc func dismissText() {
-        writeDescription.text = ""
+
+    func displayErrorMessage(title : String , message : String) { // 메시지 창
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "확인", style: .default) {
+            (action : UIAlertAction) -> Void in
+        }
+        alert.addAction(confirm)
+        present(alert, animated: true, completion: nil)
     }
+    
+    func CreateAutoCompleteHashTable() { // 해쉬태그 자동완성 팝업 뷰
+         self.HashTagview = UITableView(frame: CGRect(x: 0, y: 0, width: writeDescription.frame.width, height: self.view.frame.height/3))
+        self.HashTagview.delegate = self
+        self.HashTagview.dataSource = self
+        self.HashTagview.isScrollEnabled = false
+        self.popover = Popover(options: self.popoverOptions)
+        self.popover.willShowHandler = {
+            print("willShowHandler")
+        }
+        self.popover.didShowHandler = {
+            print("didDismissHandler")
+        }
+        self.popover.willDismissHandler = {
+            print("willDismissHandler")
+        }
+        self.popover.didDismissHandler = {
+            print("didDismissHandler")
+        }
+        self.popover.show(self.HashTagview, fromView: self.writeDescription)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        stringArray.removeAll()
         PostArray.removeAll()
         ref = Database.database().reference()
         storageRef = Storage.storage().reference()
         writeDescription.delegate = self
         writeimageView.image = writeImage
         writeDescription.tintColor = UIColor.lightGray
-        findUsers(text: "감")
+        
         let Tap = UITapGestureRecognizer(target: self, action: #selector(dismisskeyboard))
         self.view.addGestureRecognizer(Tap)
-        
+        //        let TableTap = UITapGestureRecognizer(target: self, action: #selector(dismissTable))
+        //        self.view.addGestureRecognizer(TableTap)
         ref?.child("HashTagPosts").observe(.childAdded, with: { (snapshot) in
             if snapshot.value is NSNull {
                 print("Null")
             } else {
                 
                 print(snapshot.key)
+                let str = "#" + snapshot.key
+                self.stringArray.append(str)
+                //print(str)
             }
         })
         if writeImage == nil {
@@ -207,7 +206,7 @@ class WriteViewController: UIViewController, UITextViewDelegate,UIPopoverPresent
         }
         writeDescription.snp.makeConstraints { (make) in
             make.width.equalTo(self.view.frame.width)
-            make.height.equalTo(self.view.frame.height/25)
+            make.height.equalTo(self.view.frame.height/22)
             make.top.equalTo(writeimageView.snp.bottom).offset(10)
         }
         writeDescription.adjustsFontForContentSizeCategory = true
@@ -232,28 +231,64 @@ class WriteViewController: UIViewController, UITextViewDelegate,UIPopoverPresent
         // Do any additional setup after loading the view.
     }
     
-    func displayErrorMessage(title : String , message : String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let confirm = UIAlertAction(title: "확인", style: .default) {
-            (action : UIAlertAction) -> Void in
-        }
-        alert.addAction(confirm)
-        present(alert, animated: true, completion: nil)
-    }
-    
-
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        if segue.identifier == "ii" {
-            let destination = segue.destination as! ViewController
-            destination.image = str
-        }
     }
+}
+extension WriteViewController : UITableViewDataSource {
     
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.result.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
+        cell.textLabel?.text = self.result[indexPath.row]
+        return cell
+    }
+}
+extension WriteViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("시발?")
+        print(self.writeDescription.text)
+        let selectedCell : UITableViewCell = tableView.cellForRow(at: indexPath)!
+        let selectedText = selectedCell.textLabel?.text as String!
+        let trim = self.writeDescription.text.replacingOccurrences(of: resultString, with: "", options: .literal, range: nil)
+        let trimmingString = trim.replacingOccurrences(of: "#", with: "")
+        self.writeDescription.text = trimmingString + selectedText!
+        self.popover.dismiss()
+    }
+}
+extension WriteViewController : UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        strr = self.writeDescription.text
+        let items = strr.components(separatedBy: "#")
+        if items.count > 1 {
+            resultString = items.last! + text
+            let searchPredicate = NSPredicate(format: "SELF CONTAINS %@", items.last!)
+            let array = (stringArray as NSArray).filtered(using: searchPredicate)
+            result = array as! [String]
+            if (result.isEmpty) == false {
+                
+                self.HashTagview.reloadData()
+            }
+        }
+        if text.hasPrefix("#") {
+            CreateAutoCompleteHashTable()
+            print("HashTag!!!!!")
+        }
+        if textView.text == "설명 입력" {
+            textView.text = ""
+            textView.tintColor = UIColor.black
+        }
+        if (text == "\n") {
+            textView.endEditing(true)
+            return false
+        }
+        return true
+    }
 }
