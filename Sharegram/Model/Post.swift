c1fd6a96a35abb5a381316a59d4234def60808f6
LeeHyun <korea8036@gmail.com>
2018-02-08 17:43:46 +0900

    일단 옮기고 저장

---
+++ Sharegram/Model/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  Sharegram
+//
+//  Created by 박주영 on 2018. 1. 11..
+//  Copyright © 2018년 박주영. All rights reserved.
+//
+
+import UIKit
+
+
+struct Post
+{
+    var createBy: User
+    var timeAgo: String?
+    var caption: String?
+    var image: String?
+    var numberOfLikes: Int?
+    var numberOfComments: Int?
+    var numberOfShares: Int?
+    
+  
+}
--- Sharegram/SearchViewController.swift
+++ Sharegram/SearchViewController.swift
@@ -4,18 +4,25 @@
 //
 //  Created by 박주영 on 2018. 1. 11..
 //  Copyright © 2018년 박주영. All rights reserved.
-//
+//  Write SatGatLee
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchBarDelegate {
 
+    
+    
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    @objc func Action() {
+        print("씨발")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
