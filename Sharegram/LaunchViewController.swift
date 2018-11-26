99bd8480f3c726b509ae942e024c40ae2cc5964b
LeeChangHwa <step323@naver.com>
2018-11-26 21:26:07 +0900

    제발

---
+++ Sharegram/LaunchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchViewController.swift
+//  Sharegram
+//
+//  Created by SatGatLee on 2018. 11. 9..
+//  Copyright © 2018년 박주영. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
